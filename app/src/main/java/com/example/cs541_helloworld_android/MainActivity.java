227d627832207edb1d9ebb88c004441312a54749
lalitmanwani <43362578+lalitmanwani@users.noreply.github.com>
2019-06-03 02:32:39 -0400

    Update 1.1 Change the Layout. Added  a New World image. Added a button for functionality.

--- app/src/main/java/com/example/cs541_helloworld_android/MainActivity.java
+++ app/src/main/java/com/example/cs541_helloworld_android/MainActivity.java
@@ -2,12 +2,60 @@ package com.example.cs541_helloworld_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static ImageView nameimg;
+
+    String[] namepics = {"english","hindi"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button changebutton = (Button) findViewById(R.id.button2);
+        changebutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changeimg();
+            }
+        });
+
     }
+
+
+    Random x = new Random();
+
+
+
+
+
+    public void changeimg()
+    {
+
+        int n = x.nextInt(2);
+        nameimg = (ImageView) findViewById(R.id.image);
+
+        int id = getResources().getIdentifier("com.example.cs541_helloworld_android:drawable/" + namepics[n], null, null);
+
+
+        nameimg.setImageResource(id);
+
+
+        System.out.println((n));
+
+
+
+    }
+
+
+
+
+
+
+
 }
